aebd4dbd0d4411ad94916461eb6d6d65ff7bc9d5
rterry5 <rterry5@gmail.com>
2020-05-21 17:08:40 -0700

    added split bill function

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const tipTotal = document.getElementById('tip-amount');
+const tipForm = document.getElementById('tip-form');
+const splitBill = document.getElementById('split-bill');
+const people = document.getElementById('people');
 const billTotal = document.getElementById('bill-total');
-const tipForm = document.querySelector('#tip-form');
 
-tipForm.onchange = function() {
+
+function calculateBill() {
     const bill = document.getElementById('bill').value;
     const tip = document.getElementById('tip').value;
     const tipAmount = ((tip/100) * bill).toFixed(2);
     const finalBill = (+bill + +tipAmount).toFixed(2);
     
     billTotal.innerText = `$${finalBill}`;
-    tipTotal.innerText = `$${tipAmount}`
+    tipTotal.innerText = `$${tipAmount}`;
+
+    function calculateSplit() {
+        const numberOfPeople = document.getElementById('people').value;
+        const splitPerPerson = document.getElementById('split-cost');
+        const splitCost = (+finalBill/+numberOfPeople).toFixed(2);
+    
+        if (numberOfPeople > 0) {
+            splitPerPerson.innerText = `$${splitCost}`;
+        } else {
+            splitPerPerson.innerText = `$${finalBill}`;
+        }
+    }
+    calculateSplit();
 }
+
+
+splitBill.onchange = function() {
+    const hideSplit = document.getElementById('hide-split');
+
+    if (splitBill.value === 'yes') {
+        hideSplit.style.display = 'block';
+    } else {
+        hideSplit.style.display = 'none';
+    }
+
+    const splitTotal = document.getElementById('split-total');
+    if (splitBill.value === 'yes') {
+        splitTotal.style.display = 'block';
+    } else {
+        splitTotal.style.display = 'none';
+    }
+}
+
+
+// Event Listener
+tipForm.addEventListener('input', calculateBill, true);
